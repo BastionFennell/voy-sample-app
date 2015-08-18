9eb522e2134b523f99c847a37e619446e832871a
Bastion Fennell <bastionfennell@utexas.edu>
2015-08-18 14:14:17 -0500

    Flesh out the employment model and add some fixtures

--- app/models/employment.js
+++ app/models/employment.js
@@ -1,5 +1,15 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
-  
+var Employment = DS.Model.extend({
+  name: DS.attr(),
+  monthlySalary: DS.attr()
 });
+
+Employment.reopenClass({
+ FIXTURES: [
+  {id: 1, name: "Bastion Fennell", monthlySalary: 10000},
+  {id: 2, name: "Marguerite West-Driga", monthlySalary: 12500}
+  ]
+});
+
+export default Employment;
