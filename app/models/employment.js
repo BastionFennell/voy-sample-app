65eaced93da191dd18166b1cfd4130ee21e164d7
Bastion Fennell <bastionfennell@utexas.edu>
2015-08-18 15:21:13 -0500

    Change the model to hold the yearly salary

--- app/models/employment.js
+++ app/models/employment.js
@@ -2,13 +2,13 @@ import DS from 'ember-data';
 
 var Employment = DS.Model.extend({
   name: DS.attr(),
-  monthlySalary: DS.attr()
+  salary: DS.attr()
 });
 
 Employment.reopenClass({
  FIXTURES: [
-  {id: 1, name: "Undergraduate Student", monthlySalary: 10000},
-  {id: 2, name: "Software Developer", monthlySalary: 12500}
+  {id: 1, name: "Undergraduate Student", salary: 10000},
+  {id: 2, name: "Software Developer", salary: 12500}
   ]
 });
 
