398e7bef18d0d043be0932d2aec0828d37eb8959
Bastion Fennell <bastionfennell@utexas.edu>
2015-08-18 15:17:38 -0500

    Change the fixtures to something that makes sense

--- app/models/employment.js
+++ app/models/employment.js
@@ -7,8 +7,8 @@ var Employment = DS.Model.extend({
 
 Employment.reopenClass({
  FIXTURES: [
-  {id: 1, name: "Bastion Fennell", monthlySalary: 10000},
-  {id: 2, name: "Marguerite West-Driga", monthlySalary: 12500}
+  {id: 1, name: "Undergraduate Student", monthlySalary: 10000},
+  {id: 2, name: "Software Developer", monthlySalary: 12500}
   ]
 });
 
