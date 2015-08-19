b790765a3d5178567edb3b1ff57b577745bd4fb5
Bastion Fennell <bastionfennell@utexas.edu>
2015-08-18 23:08:06 -0500

    Flesh out the actions

--- app/controllers/employment.js
+++ app/controllers/employment.js
@@ -1,15 +1,65 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  currentEmployment: null,
+  name: "",
+  salary: "",
+  yearly: "",
+
   actions: {
+    cancel: function(){
+      this.set("currentEmployment", null);
+      this.set("name", "");
+      this.set("salary", "");
+    },
     save: function(){
-      console.log("save");
+      var self = this;
+      if(this.get("currentEmployment")){
+        if(isNaN(this.get("yearly"))){
+          console.log("Yearly salary is NaN");
+          return;
+        }
+        if(this.get("name") === ""){
+          return;
+        }
+
+        var currentEmployment = this.get("currentEmployment");
+        currentEmployment.set("name", this.get("name"));
+        currentEmployment.set("salary", this.get("salary"));
+        currentEmployment.save().then(function(){
+          self.set("currentEmployment", null);
+          self.set("name", "");
+          self.set("salary", "");
+        });
+      }
+      else {
+        if(isNaN(this.get("yearly"))){
+          console.log("Yearly salary is NaN");
+          return;
+        }
+        if(this.get("name") === ""){
+          return;
+        }
+
+        var newEmployment = this.store.createRecord("employment");
+        newEmployment.set("name", this.get("name"));
+        newEmployment.set("salary", this.get("yearly"));
+        newEmployment.save().then(function(){
+          self.set("currentEmployment", null);
+          self.set("name", "");
+          self.set("salary", "");
+        });
+      }
     },
-    edit: function(){
-      console.log("edit");
+    edit: function(employment){
+      this.set("currentEmployment", employment);
+      this.set('name', employment.get("name"));
+      this.set('salary', employment.get("salary"));
     },
-    delete: function(){
-      console.log("delete");
+    delete: function(employment){
+      if(confirm("Are you sure you want to delete " + employment.get("name"))){
+        employment.destroyRecord();
+      }
     }
   }
 });
