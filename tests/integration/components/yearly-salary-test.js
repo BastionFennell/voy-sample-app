760a7b92e84ea2d1af358ee02b035ec9f3a7ae64
Bastion Fennell <bastionfennell@utexas.edu>
2015-08-18 23:41:18 -0500

    Make the yearly salary component tests more robust

--- tests/integration/components/yearly-salary-test.js
+++ tests/integration/components/yearly-salary-test.js
@@ -15,3 +15,33 @@ test('it renders', function(assert) {
 
   assert.equal(this.$().text().trim(), 'Yearly Salary: 0');
 });
+
+test('it correctly converts to yearly salary if it is an integer', function(assert) {
+  this.render(hbs`{{yearly-salary monthly=10}}`);
+
+  assert.equal(this.$().text().trim(), 'Yearly Salary: 120');
+});
+
+test('it correctly converts to yearly salary if it is a string', function(assert) {
+  this.render(hbs`{{yearly-salary monthly="10"}}`);
+
+  assert.equal(this.$().text().trim(), 'Yearly Salary: 120');
+});
+
+test('it displays an error if the yearly salary is not a number', function(assert) {
+  this.render(hbs`{{yearly-salary monthly="doot"}}`);
+
+  assert.equal(this.$().text().trim(), 'Yearly Salary: Please put in a valid monthly salary');
+});
+
+test('it correctly converts to yearly salary if it has a dollar sign', function(assert) {
+  this.render(hbs`{{yearly-salary monthly="$10"}}`);
+
+  assert.equal(this.$().text().trim(), 'Yearly Salary: 120');
+});
+
+test('it correctly converts to yearly salary if it has a dollar sign and commas', function(assert) {
+  this.render(hbs`{{yearly-salary monthly="$10,000"}}`);
+
+  assert.equal(this.$().text().trim(), 'Yearly Salary: 120000');
+});
