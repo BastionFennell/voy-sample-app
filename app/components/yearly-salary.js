65e56cc7122be0a09028e61921465ea48daca5ec
Bastion Fennell <bastionfennell@utexas.edu>
2015-08-18 23:09:06 -0500

    Make the yearly-salary app cleaner

--- app/components/yearly-salary.js
+++ app/components/yearly-salary.js
@@ -2,21 +2,27 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   monthly: 0,
+  output: 0,
   salary: function(){
     var cleanMonthly = this.get("monthly") || '0';
-    var monthlyInt;
-    if(cleanMonthly.charAt(0) == '$'){
-      cleanMonthly = cleanMonthly.substring(1);
+    if(isNaN(parseInt(cleanMonthly))){
+
+      var monthlyInt;
+      if(cleanMonthly.charAt(0) === '$'){
+        cleanMonthly = cleanMonthly.substring(1);
+      }
+
+      cleanMonthly = cleanMonthly.replace(/,/,'');
+
+      monthlyInt = parseInt(cleanMonthly);
+
+      if(isNaN(monthlyInt)){
+        this.set('output', NaN);
+        return "Please put in a valid monthly salary";
+      }
     }
 
-    cleanMonthly = cleanMonthly.replace(/,/,'');
-
-    monthlyInt = parseInt(cleanMonthly);
-
-    if(isNaN(monthlyInt)){
-      return "Please put in a valid monthly salary";
-    }
-
+    this.set("output", cleanMonthly * 12);
     return cleanMonthly * 12;
   }.property("monthly")
 });
