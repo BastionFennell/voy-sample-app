cfc4a10cf1c7a0f3f1b5e86e4200f925d6564db3
Bastion Fennell <bastionfennell@utexas.edu>
2015-08-18 23:13:10 -0500

    update the yearly-salary unit test

--- tests/integration/components/yearly-salary-test.js
+++ tests/integration/components/yearly-salary-test.js
@@ -6,21 +6,12 @@ moduleForComponent('yearly-salary', 'Integration | Component | yearly salary', {
 });
 
 test('it renders', function(assert) {
-  assert.expect(2);
+  assert.expect(1);
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
   this.render(hbs`{{yearly-salary}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#yearly-salary}}
-      template block text
-    {{/yearly-salary}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$().text().trim(), 'Yearly Salary: 0');
 });
